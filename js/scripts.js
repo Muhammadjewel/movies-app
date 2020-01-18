b33f63aeae5e221e24de2c415369786873560fc6
muhammadjewel <msuratov@gmail.com>
2020-01-18 15:20:15 +0500

    Create helper functions for movie elements creation

--- js/scripts.js
+++ js/scripts.js
@@ -26,14 +26,42 @@ var titleInputElement = document.querySelector('#title-input');
 var genreSelectElement = document.querySelector('#genre-select');
 var minYearElement = document.querySelector('#min-year-input');
 var maxYearElement = document.querySelector('#max-year-input');
+
 // Movies List
 var moviesListElement = document.querySelector('.movies__list');
 var movieTemplate = document.querySelector('#movie-template').content;
+
 // Watchlist
 var watchlistElement = document.querySelector('.watchlist__list');
 var watchlistItemTemplate = document.querySelector('#watchlist-item-template').content;
+
 // Modal
 var modalElement = document.querySelector('.modal');
 var modalCloseButtonElement = document.querySelector('.modal__close-button');
 var modalMovieTitleElement = document.querySelector('.modal__movie-title');
 var modalMovieSummaryElement = document.querySelector('.modal__movie-summary');
+
+// Helper DOM functions
+var createMovieElement = function (movie) {
+  var movieElement = document.importNode(movieTemplate, true);
+
+  movieElement.querySelector('.movie__title').textContent = movie.title;
+  movieElement.querySelector('.movie__rating').textContent = movie.imdb_rating;
+  movieElement.querySelector('.movie__year').textContent = movie.year;
+  movieElement.querySelector('.movie__genres').textContent = movie.genres.join(', ');
+  movieElement.querySelector('.movie__trailer-link').href += movie.youtube_id;
+  movieElement.querySelector('.movie__summary-button').dataset.summary = movie.summary;
+  movieElement.querySelector('.movie__bookmark').dataset.id = movie.id;
+
+  return movieElement;
+};
+
+var createMoviesFragmentElement = function (movies) {
+  var moviesFragment = document.createDocumentFragment();
+
+  movies.forEach(function (movie) {
+    moviesFragment.appendChild(createMovieElement(movie));
+  });
+
+  return moviesFragment;
+}
